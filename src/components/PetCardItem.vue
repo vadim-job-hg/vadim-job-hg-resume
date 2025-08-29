e34ad87ac8ec5ec49f0a8538c8a49ae787a98b8c
Vadym Kuznetsov <v.kuznetsov@Lc-Company.de>
2025-08-29 19:17:33 +0300

    add number and fix icons

---
+++ src/components/PetCardItem.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import type { Pet } from '@/models/Pet';
+import HeroIcon from './HeroIcon.vue';
+import { getImagePath } from '@/helpers/Helper';
+
+defineProps<{ item: Pet; separator: boolean }>();
+</script>
+
+<template>
+  <div class="mb-5 flex items-start">
+    <img
+      :src="getImagePath(item.image)"
+      alt="pet project logo"
+      class="h-14 w-14 shrink-0 rounded-xl border-2 border-gray-50 shadow-sm dark:border-night-700" />
+    <div class="ml-3 w-full space-y-5">
+      <div class="flex justify-between">
+        <div class="space-y-1.5">
+          <div class="font-medium dark:text-night-50">{{ item.name }}</div>
+          <div v-if="item.language" class="flex items-center gap-1 text-sm font-medium text-gray-400 dark:text-night-400">
+            <HeroIcon :icon="'CodeBracketIcon'" class="h-4 w-4 shrink-0 stroke-2" />
+            <span>{{ item.language }}</span>
+          </div>
+        </div>
+      </div>
+      <p class="text-sm text-gray-600 dark:text-night-300">
+        {{ item.description }}
+      </p>
+      <div class="flex flex-wrap gap-2">
+        <a
+          v-for="(link, i) in item.url"
+          :key="i"
+          :href="link"
+          target="_blank"
+          class="inline-flex cursor-pointer items-center gap-1 rounded-lg border-2 border-gray-100 px-2 py-1.5 text-xs font-medium text-gray-400 hover:border-primary-500 hover:bg-primary-500 hover:text-white dark:border-night-700 dark:text-night-400 dark:hover:border-primary-500 dark:hover:bg-primary-500/20 dark:hover:text-primary-500">
+          <HeroIcon :icon="'LinkIcon'" class="h-4 w-4 shrink-0 stroke-2" />
+          <span>Link {{ i + 1 }}</span>
+        </a>
+      </div>
+      <div v-if="separator" class="border-b border-dashed border-gray-200 dark:border-night-600"></div>
+    </div>
+  </div>
+</template>
--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,39 +1,67 @@
 <script setup lang="ts">
 import { computed, ref } from 'vue';
 import ProjectCardItem from './ProjectCardItem.vue';
+import PetCardItem from './PetCardItem.vue';
 import { useResumeStore } from '@/stores/ResumeStore';
 import { storeToRefs } from 'pinia';
 import MyPaginate from './MyPaginate.vue';
 
 const itemsPerPage = 3;
 const currentPage = ref(1);
+const currentPetPage = ref(1);
+
 const resumeStore = useResumeStore();
 const { resume } = storeToRefs(resumeStore);
 
-const total = computed(() => {
-  return resume.value.project.length;
-});
+const totalProjects = computed(() => resume.value.project.length);
+const totalPets = computed(() => resume.value.pets.length);
 
-const paginatedItems = computed(() => {
+const paginatedProjects = computed(() => {
   const start = (currentPage.value - 1) * itemsPerPage;
   const end = start + itemsPerPage;
   return resume.value.project.slice(start, end);
 });
 
-function onPageChange(page: number) {
+const paginatedPets = computed(() => {
+  const start = (currentPetPage.value - 1) * itemsPerPage;
+  const end = start + itemsPerPage;
+  return resume.value.pets.slice(start, end);
+});
+
+function onProjectPageChange(page: number) {
   currentPage.value = page;
 }
+function onPetPageChange(page: number) {
+  currentPetPage.value = page;
+}
 </script>
+
 <template>
   <div class="card">
+    <!-- PROJECTS -->
     <h2 class="mb-5 text-lg font-semibold dark:text-night-50">{{ $t('project') }}</h2>
-    <!-- Project 1 -->
     <ProjectCardItem
-      v-for="(item, index) of paginatedItems"
+      v-for="(item, index) of paginatedProjects"
       :item="item"
-      :key="index"
-      :separator="paginatedItems.length > index + 1" />
-    <!-- Paginate  -->
-    <MyPaginate :totalItems="total" :itemsPerPage="itemsPerPage" :maxVisibleButtons="3" @pagechanged="onPageChange" />
+      :key="`proj-${index}`"
+      :separator="paginatedProjects.length > index + 1" />
+    <MyPaginate
+      :totalItems="totalProjects"
+      :itemsPerPage="itemsPerPage"
+      :maxVisibleButtons="3"
+      @pagechanged="onProjectPageChange" />
+
+    <!-- PET PROJECTS -->
+    <h2 class="mt-10 mb-5 text-lg font-semibold dark:text-night-50">{{ $t('pet_projects') }}</h2>
+    <PetCardItem
+      v-for="(item, index) of paginatedPets"
+      :item="item"
+      :key="`pet-${index}`"
+      :separator="paginatedPets.length > index + 1" />
+    <MyPaginate
+      :totalItems="totalPets"
+      :itemsPerPage="itemsPerPage"
+      :maxVisibleButtons="3"
+      @pagechanged="onPetPageChange" />
   </div>
 </template>
